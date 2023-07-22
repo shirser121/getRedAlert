403163404194560e293347acbcc0735b7f27fb7b
Shir Serlui <70711723+shirser121@users.noreply.github.com>
2023-07-22 22:59:49 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+import requests
+import threading
+import time
+
+app = Flask(__name__)
+apiUrl = 'https://api.tzevaadom.co.il/alerts-history/'
+jsonData = None
+
+def updateData():
+    global jsonData
+    while True:
+        try:
+            response = requests.get(apiUrl)
+            jsonData = response.json()
+        except Exception as e:
+            print(f'Error: {e}')
+        time.sleep(10)
+
+@app.route('/')
+def home():
+    return jsonify(jsonData)
+
+if __name__ == '__main__':
+    updateThread = threading.Thread(target=updateData)
+    updateThread.start()
+    app.run(port=3000)
